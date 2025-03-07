5a3f61ae144d5293c6c0ecf91891f3a1dc681877
Océane Bertrand <oceanebertrand339@gmail.com>
2025-03-07 16:19:28 +0100

    ♻️ Update header component tests

---
+++ src/tests/componentsTest/headerTest/Header.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { describe, expect, it, beforeEach } from 'vitest';
+import Header from '../../../components/Header/Header';
+
+const setup = () => {
+  render(
+    <BrowserRouter>
+        <Header />
+    </BrowserRouter>
+  );
+};
+
+describe('Header component', () => {
+    beforeEach(() => {
+        setup();
+    });
+
+    it('renders links', () => {
+        const links = ['Accueil', 'Inscription', 'Connexion', 'Créer une sauce'];
+        links.forEach((link) => {
+            expect(screen.getByText(link)).toBeInTheDocument();
+        });
+    });
+
+    it('renders links with the correct href attributes', () => {
+        const links = [
+            { text: 'Accueil', href: '/' },
+            { text: 'Inscription', href: '/register' },
+            { text: 'Connexion', href: '/login' },
+            { text: 'Créer une sauce', href: '/create-sauce' },
+        ];
+    
+        links.forEach(({ text, href }) => {
+            expect(screen.getByText(text).closest('a')).toHaveAttribute('href', href);
+        });
+    });
+});
