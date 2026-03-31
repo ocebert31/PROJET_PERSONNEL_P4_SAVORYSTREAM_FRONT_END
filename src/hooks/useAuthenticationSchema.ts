c452fb51fd0a228aac649516639db8d49eb1a901
ocebert31 <oceanebertrand339@gmail.com>
2026-03-31 21:45:52 +0200

    Fix types TS in authentication

--- src/hooks/useAuthenticationSchema.ts
+++ src/hooks/useAuthenticationSchema.ts
@@ -3,9 +3,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { AuthenticationSchema } from "../schemas/AuthenticationSchema";
 import type { LoginFormData, RegisterFormData } from "../types/User";
 
-export function useAuthenticationSchema(isLoginPage: true): ReturnType<typeof useForm<LoginFormData>>;
-export function useAuthenticationSchema(isLoginPage: false): ReturnType<typeof useForm<RegisterFormData>>;
-export function useAuthenticationSchema(isLoginPage: boolean) {
+export function useAuthenticationSchema(isLoginPage: boolean): ReturnType<typeof useForm<LoginFormData | RegisterFormData>> {
   const schema = AuthenticationSchema(isLoginPage);
   return useForm<LoginFormData | RegisterFormData>({
     resolver: yupResolver(schema),
